2964f20fba7c46a8bbb4d71a2878615e13f3afc8
Tristan Dhuisme <tristan.dhuisme@gmail.com>
2025-01-24 10:21:31 +0100

    add files

---
+++ src/test/java/td/rentalservice/RentalserviceApplicationTests.java
@@ -0,0 +1,20 @@
+package td.rentalservice;
+
+import td.rentalService.data.Car;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class RentalserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void test1(){
+		Car car = new Car("11AA22", 1000);
+		assertEquals(car.getPlateNumber(), "11AA22");
+	}
+}
